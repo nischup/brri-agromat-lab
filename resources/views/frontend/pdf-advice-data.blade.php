276d749e2510fe44fd209542796ff870f8fb645c
nischup <ahsabbir23@gmail.com>
2023-01-24 00:46:44 +0600

    pdf design updated

--- resources/views/frontend/pdf-advice-data.blade.php
+++ resources/views/frontend/pdf-advice-data.blade.php
@@ -13,44 +13,186 @@
     body {
         font-family: 'Kalpurush', sans-serif;
     }
+
+    table, td, th {
+      border: 1px solid black;
+    }
+
+    table {
+      border-collapse: collapse;
+/*      width: 100%;*/
+    }
+
 </style>
 
 <body>
-
-    <section class="login-area" style="margin-top: 100px;">
+    <section class="pdf-area">
         <div class="container">
             <div class="row">
-                <div class="col-md-2"></div>
-                <div class="col-md-8">
-                  <div class="row">
-                        <div class="col-md-2">
-                            <img class="newable-logo" 
-                                src="{{ public_path('images/bd-logo.png') }}" alt="{{ public_path('images/bd-logo.png') }}" width="100" height="100" />
-                        </div>
-                        <div class="col-md-8">
-                                <h3 class="text-center"> গনপ্রজাতন্ত্রী বাংলাদেশ সরকার </h3>
-                                <p class="text-center">কৃষি মন্ত্রণালয়<br> বাংলাদেশ ধান গবেষণা ইনিস্টিটিউট <br> এগ্রোমেট ল্যাব</p>
-                        </div>
-                        <div class="col-md-2">
-                             <img class="newable-logo" 
-                                src="{{ public_path('images/brri-logo.jpg') }}"
-                                alt="{{ public_path('images/brri-logo.jpg') }}" width="100" height="100" />
-                        </div>
-                  </div>
+                <div class="col-md-2">
+                    <img style="margin-left:-10px;" class="newable-logo" 
+                        src="{{ public_path('images/bd-logo.png') }}" alt="{{ public_path('images/bd-logo.png') }}" width="100" height="100" />
+
+                        <img style="margin-left:440px; margin-top:-100px;" class="newable-logo" 
+                        src="{{ public_path('images/brri-logo.jpg') }}"
+                        alt="{{ public_path('images/brri-logo.jpg') }}" width="100" height="100" />
                 </div>
-                <div class="col-md-2"></div>
+                <div class="col-md-8" style="margin-top:-120px;" >
+                        <h3 class="text-center"> গনপ্রজাতন্ত্রী বাংলাদেশ সরকার </h3>
+                        <p class="text-center">কৃষি মন্ত্রণালয়<br> বাংলাদেশ ধান গবেষণা ইনিস্টিটিউট <br> এগ্রোমেট ল্যাব</p>
+                        <p class="text-center"> <a href=""> https://iras.brri.gov.bd/ </a> </p>
+                </div>
+                <div class="col-md-2" ></div>
             </div>
            
             <div class="row">
-                <div class="col-md-2"></div>
-                <div class="col-md-8">
-                </div>
-                <div class="col-md-2"></div>
+                   <table style="width: 60%; margin-left: 105px;">
+                      <tr style="height: 30px;">
+                        <th style="width: 200px;">Bisesh Krisi Poramorso</th>
+                        <th></th>
+                      </tr>
+                    </table>                  
+                    <br>
+                    <table style="width: 100%">
+                      <tr style="height: 30px;">
+                        <th style="width: 250px;">Sarok Potro no: brikrip-001</th>
+                        <th>10/01/2023</th>
+                      </tr>
+                    </table>                    
+                    <br>
+                    <table style="width: 70%; margin-left: 80px;">
+                      <tr style="height: 30px;">
+                        <th style="width: 200px;">Purbavaser Somoykal</th>
+                        <th></th>
+                      </tr>
+                    </table>                    
+                    <br>
+                    <table style="width: 30%;">
+                      <tr style="height: 30px;">
+                        <th style="width: 210px;"> Purbavaser Sar Songkhep: </th>
+                      </tr>
+                    </table>                    
+                    <table style="width: 100%; margin-top:10px;">
+                      <tr style="height: 70px;">
+                        <th style="height: 70px;">  </th>
+                      </tr>
+                    </table>                    
+
+                    <br>
+                    <table style="width: 30%;">
+                      <tr style="height: 30px;">
+                        <th style="width: 210px;"> Map: </th>
+                      </tr>
+                    </table>                    
+                    <table style="width: 100%; margin-top:10px;">
+                      <tr style="height: 70px;">
+                        <th style="height: 70px;">  </th>
+                      </tr>
+                    </table>                    
+
+                    <br>
+                    <table style="width: 30%;">
+                      <tr style="height: 30px;">
+                        <th style="width: 210px;"> Graph: </th>
+                      </tr>
+                    </table>                    
+                    <table style="width: 100%; margin-top:10px;">
+                      <tr style="height: 70px;">
+                        <th style="height: 70px;">  </th>
+                      </tr>
+                    </table>
+                    <br>
+                    <table style="width: 30%;">
+                      <tr style="height: 30px;">
+                        <th style="width: 210px;"> Table: </th>
+                      </tr>
+                    </table>                    
+                    <table style="width: 100%; margin-top:10px;">
+                      <tr style="height: 70px;">
+                        <th style="height: 70px;">  </th>
+                      </tr>
+                    </table>                  
+                    <br>
+                    <table style="width: 30%;">
+                      <tr style="height: 30px;">
+                        <th style="width: 210px;"> Sms Krsihi Poramorso: </th>
+                      </tr>
+                    </table>                    
+                    <table style="width: 100%; margin-top:10px;">
+                      <tr style="height: 70px;">
+                        <th style="height: 70px;">  </th>
+                      </tr>
+                    </table>                    
+                    <br>
+                    <table style="width: 50%; margin-left: 50px;">
+                      <tr style="height: 30px;">
+                        <th style="width: 210px;"> Dhan Gacher Briddi Porjay: </th>
+                        <th style="width: 210px;"> </th>
+                      </tr>
+                    </table>                    
+                    <table style="width: 100%; margin-top:10px;">
+                      <tr style="height: 70px;">
+                        <th style="height: 70px;">  </th>
+                      </tr>
+                    </table>                    
+                    <br>
+                    <table style="width: 30%;">
+                      <tr style="height: 30px;">
+                        <th style="width: 210px;"> Krisi Tattik Bebosthapona: </th>
+                      </tr>
+                    </table>                    
+                    <table style="width: 100%; margin-top:10px;">
+                      <tr style="height: 70px;">
+                        <th style="height: 70px;">  </th>
+                      </tr>
+                    </table>                    
+                    <br>
+                    <table style="width: 30%;">
+                      <tr style="height: 30px;">
+                        <th style="width: 210px;"> Krisi Tattik Bebosthapona: </th>
+                      </tr>
+                    </table>                    
+                    <table style="width: 100%; margin-top:10px;">
+                      <tr style="height: 70px;">
+                        <th style="height: 70px;">  </th>
+                      </tr>
+                    </table>                    
+                    <br>
+                    <table style="width: 30%;">
+                      <tr style="height: 30px;">
+                        <th style="width: 210px;"> Sesh Pani Bebosthapona: </th>
+                      </tr>
+                    </table>                    
+                    <table style="width: 100%; margin-top:10px;">
+                      <tr style="height: 70px;">
+                        <th style="height: 70px;">  </th>
+                      </tr>
+                    </table>                    
+                    <br>
+                    <table style="width: 30%;">
+                      <tr style="height: 30px;">
+                        <th style="width: 210px;"> Rog Balai Bebosthapona: </th>
+                      </tr>
+                    </table>                    
+                    <table style="width: 100%; margin-top:10px;">
+                      <tr style="height: 70px;">
+                        <th style="height: 70px;">  </th>
+                      </tr>
+                    </table>                    
+                    <br>
+                    <table style="width: 30%;">
+                      <tr style="height: 30px;">
+                        <th style="width: 220px;"> Poka Makor Bebosthapona: </th>
+                      </tr>
+                    </table>                    
+                    <table style="width: 100%; margin-top:10px;">
+                      <tr style="height: 70px;">
+                        <th style="height: 70px;">  </th>
+                      </tr>
+                    </table>
+         
             </div>
         </div>
     </section>
-
-    @livewireScripts
-
 </body>
 </html>
